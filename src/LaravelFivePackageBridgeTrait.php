a64d714d4948c2f59a9a684dc5f32b733ffb1bad
Craig Morris <craig.michael.morris@gmail.com>
2015-01-19 16:16:16 +0000

    Update for removal of config->package

--- src/LaravelFivePackageBridgeTrait.php
+++ src/LaravelFivePackageBridgeTrait.php
@@ -8,6 +8,7 @@ trait LaravelFivePackageBridgeTrait {
 	{
 		$this->loadViewsFrom($this->namespace, $this->guessPackagePath() . '/views');
 		$this->loadTranslationsFrom($this->namespace, $this->guessPackagePath() . '/lang');
+		$this->setConfig();
 		$this->app['config']->package($this->vendor . '/' . $this->namespace, $this->guessPackagePath() . '/config', $this->namespace);
 	}
 
@@ -15,6 +16,11 @@ trait LaravelFivePackageBridgeTrait {
 		$path = (new ReflectionClass(get_parent_class()))->getFileName();
 
 		return realpath(dirname($path).'/../../');
+	}
+
+	protected function setConfig() {
+		$config = require $this->guessPackagePath() . '/config/config.php';
+		$this->app['config']->set($this->namespace, $config);
 	}
 
 }
