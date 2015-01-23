5267cfa25d486aaf1337b42930c70e565e953bf9
Craig Morris <craig.michael.morris@gmail.com>
2015-01-23 11:07:01 +0000

    Add additional methods to Application

--- src/Application.php
+++ src/Application.php
@@ -5,6 +5,24 @@ use Illuminate\Foundation\Application as IlluminateApplication;
 class Application extends IlluminateApplication {
 
     /**
+     * The array of shutdown callbacks.
+     *
+     * @var array
+     */
+    protected $shutdownCallbacks = array();
+
+    /**
+     * Register a "before" application filter.
+     *
+     * @param  \Closure|string  $callback
+     * @return void
+     */
+    public function before($callback)
+    {
+        return $this['router']->before($callback);
+    }
+
+    /**
      * Register an "after" application filter.
      *
      * @param  \Closure|string  $callback
@@ -13,6 +31,24 @@ class Application extends IlluminateApplication {
     public function after($callback)
     {
         return $this['router']->after($callback);
+    }
+
+    /**
+     * Register a "shutdown" callback.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public function shutdown(callable $callback = null)
+    {
+        if (is_null($callback))
+        {
+            $this->fireAppCallbacks($this->shutdownCallbacks);
+        }
+        else
+        {
+            $this->shutdownCallbacks[] = $callback;
+        }
     }
 
 }
