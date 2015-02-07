1c81406810c90d4bad5da1fbd816a5d15bc8b460
Ashley Breeden <ash@url.net.au>
2015-02-07 15:57:29 +1100

    Removed Laracasts/Utilities as this is now supported in L5 if using -  "laracasts/utilities": "~2.0"

--- src/Bridges/UtilitiesServiceProvider.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Morrislaptop\LaravelFivePackageBridges\Bridges;
-
-use Laracasts\Utilities\UtilitiesServiceProvider as BaseUtilitiesServiceProvider;
-use Morrislaptop\LaravelFivePackageBridges\LaravelFivePackageBridgeTrait;
-
-class UtilitiesServiceProvider extends BaseUtilitiesServiceProvider {
-	use LaravelFivePackageBridgeTrait;
-}
